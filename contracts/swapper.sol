297298524d532904769ea6659de4ec26b025281d
ulbdire <ulbdir@gmx.de>
2022-05-28 21:19:49 +0200

    First draft of swapper contract complete

--- contracts/swapper.sol
+++ contracts/swapper.sol
@@ -14,81 +14,79 @@ contract Swapper is Ownable {
     // address of WFTM Token
     address private wftm = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
 
-
-    // This contract needs to be pre-approved to withdraw wftm from the caller!
-    // 1. transfer wftm from wallet to contract
-    // 2. swap WFTM for token at r1
-    // 3. swapp token for WFTM at r2
-    // 4. check if swap was profitable
-    //  a) if not profitable: revert tx
-    //  b) if profitable: send wftm back to wallet
+    // transfer wftm from wallet to contract
+    // swap WFTM for token at r1
+    // swapp token for WFTM at r2
+    // check if swap was profitable
+    //  a) not profitable: revert tx
+    //  b) profitable: send wftm back to wallet
     function multiswap(address r1, address r2, address token, uint256 amount) external onlyOwner {
         
         IERC20 token_wftm = IERC20(wftm);
     
-        // 1. transfer the WFTM from the wallet to the contract
+        // transfer the WFTM from the wallet to the contract
         token_wftm.transferFrom(msg.sender, address(this), amount);
     
         // keep track of initial balance of wftm
         uint _balance_initial = token_wftm.balanceOf(address(this));
         
-        // 2. swap wftm for token at r1
+        // do the buying and selling of the token
         uint _amount = buy(r1, token, amount);
-        
-        // 3. swap token for wftm at r2
         sell(r2, token, _amount);
 
         // check new balance of wftm
         uint _balance_after_swap = token_wftm.balanceOf(address(this));
 
-        // 4.a) revert tx if the swap was not profitable
+        // revert tx if the swap was not profitable
         require(_balance_after_swap > _balance_initial, "Not profitable");
 
-        // 4.b) transfer WFTM back to wallet
+        // transfer WFTM back to wallet
         token_wftm.transferFrom(address(this), msg.sender, _balance_after_swap);
     }
 
-    function buy(address _router, address _token, uint _amount) private returns(uint256) {
-        IERC20 token_wftm = IERC20(wftm);
-        IERC20 token_swap = IERC20(_token);
+    function swap(address _router, address _token_in, address _token_out, uint _amount_in) private returns (uint) {
+        address[] memory path = new address[](2);
+        path[0] = _token_in;
+        path[1] = _token_out;
 
-        // approve _router to deduct _amount of _wftm from this contract
-        IERC20(token_wftm).approve(_router, _amount);
+        // approve _router to deduct _amount_in of _token_in from this contract
+        IERC20(_token_in).approve(_router, _amount_in);
 
-        // TODO: use uniswap router interface to execute the swap
+        // set deadline to 1h
+        uint deadline = block.timestamp + 3600;
+
+        uint amount_received = IUniswapV2Router01(_router).swapExactTokensForTokens(_amount_in, 0, path, address(this), deadline)[1];
+        return amount_received;
     }
 
-    function sell(address _router, address _token, uint _amount) private {
-        IERC20 token_wftm = IERC20(wftm);
-        IERC20 token_swap = IERC20(_token);
-
-        // approve _router to deduct _amount of _token from this contract
-        IERC20(_token).approve(_router, _amount);
-
-        // TODO: use uniswap router interface to execute the swap
+    function buy(address _router, address _token, uint _amount) private returns(uint) {
+        uint amount_received = swap(_router, wftm, _token, _amount);
+        return amount_received;
     }
 
-    // this contract can receive ftm. Just in case.
+    function sell(address _router, address _token, uint _amount) private returns (uint) {
+        uint amount_received = swap(_router, _token, wftm, _amount);
+        return amount_received;
+    }
+
     receive() external payable {
     }
 
-    // View token balance of contract, in case anything went wrong. Should never be needed,
-    // since the tx is either reverted or the wftm is sent back to the caller.
-    function get_balance (address _token) external view  returns (uint256) {
-        uint balance = IERC20(_token).balanceOf(address(this));
+    // View token balance of contract, in case anything went wrong. Should never be needed.
+    function getBalance (address tc1) external view  returns (uint256) {
+        uint balance = IERC20(tc1).balanceOf(address(this));
         return balance;
     }
 
-    // withdraw FTM to owner. Should never be needed.
-    function withdraw() external onlyOwner {
+    // withdraw all FTM to owner. Should never be needed.
+    function withdrawNative() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    // Withdraw token to owner. Should never be needed.
-    function withdraw_tokens(address _token) external onlyOwner {
+    // Withdraw all _token to owner. Should never be needed.
+    function withdrawTokens(address _token) external onlyOwner {
         IERC20 token = IERC20(_token);
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
+
 }
-
-
